402e5192625d0f41b5ea7f3be37d854ac290ed9e
Joshua Williams <jmw0426@hotmail.com>
2020-03-25 01:22:40 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -5,7 +5,11 @@ until levitation_force == 10
   
   puts "Wingardium Leviosa."
   
+<<<<<<< HEAD
   levitation_force += 1    
+=======
+  levitation_force =+ 1    
+>>>>>>> c62e866836508f9e549b5c416cb9788d7b250a22
  end 
 end
 
--- lib/while.rb
+++ lib/while.rb
@@ -5,7 +5,11 @@ def using_while
   
   puts "Wingardium Leviosa."
   
+<<<<<<< HEAD
   levitation_force += 1
+=======
+  levitation_force =+ 1
+>>>>>>> c62e866836508f9e549b5c416cb9788d7b250a22
 end
 end
 
